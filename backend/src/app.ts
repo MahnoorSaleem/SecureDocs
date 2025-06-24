f03373e1838093af4bf353e029f893ca3aaee0ea
MahnoorSaleem <mhnrsaleem579@gmail.com>
2025-06-24 11:29:19 +0400

    basic backend server setup

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import  express, {Request, Response} from "express";
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/', (req: Request, res: Response)=>{
+    res.send('This is my basic server');
+})
+
+export default app;
