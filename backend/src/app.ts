b3e195dcdb94a2a118e9f2c94e313eecd1e8ac56
MahnoorSaleem <mhnrsaleem579@gmail.com>
2025-07-01 12:24:32 +0400

    express limiter added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.routes';
 import documentRoutes from './routes/document.routes';
 import { globalErrorHandler } from './middlewares/error.middleware';
 import config, {corsOrigin as allowedOrigin} from './config/config';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
@@ -25,6 +26,14 @@ if (config.nodeEnv === 'production') {
     }),
   );
 }
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+    max: 100, // Limit each IP to 100 requests per `window`
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+app.use(limiter);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/docs', documentRoutes);
