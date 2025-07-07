c8eb370c97a4642b3c2b8fed99d840324352922b
MahnoorSaleem <mhnrsaleem579@gmail.com>
2025-07-07 11:24:00 +0400

    code improvement

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import documentRoutes from './routes/document.routes';
 import { globalErrorHandler } from './middlewares/error.middleware';
-import config, {corsOrigin as allowedOrigin} from './config/config';
+import config, { corsOrigin as allowedOrigin } from './config/config';
 import { customRateLimiter } from './middlewares/rateLimiter';
 import helmet from 'helmet';
 
@@ -13,21 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
-if (config.nodeEnv === 'production') {
-  app.use(
-    cors({
-      origin: allowedOrigin,
-      credentials: true,
-    }),
-  );
-} else {
-  app.use(
-    cors({
-      origin: allowedOrigin,
-      credentials: true,
-    }),
-  );
-}
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  }),
+);
 app.use(customRateLimiter);
 app.use('/api/auth', authRoutes);
 app.use('/api/docs', documentRoutes);
