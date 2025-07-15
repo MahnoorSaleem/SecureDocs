8c1b674d7f140d96a9af0742d85ed3b5247501ef
MahnoorSaleem <mhnrsaleem579@gmail.com>
2025-07-15 15:18:25 +0400

    header button condition based on path

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-50 text-center text-gray-400 text-sm py-4">
-      © 2025 SecureShare. All Rights Reserved.
+      © 2025 SecureDocs. All Rights Reserved.
     </footer>
   );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { FaLock } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 
 export default function Header() {
   const isAuth = true; // todo - state that tells user is logged in or not
   const router = useRouter();
-
+  const pathName = usePathname();
+  
   const handleLoginClick = () => {
     router.push("/login");
   };
@@ -19,13 +20,13 @@ export default function Header() {
           <FaLock className="text-blue-500 mr-2" />
           <h1 className="text-xl font-bold">SecureDocs</h1>
         </div>
-        <button
+        {pathName === '/' ? <button
           type="button"
           onClick={handleLoginClick}
           className="bg-lue-500 text-blue px-4 py-1 border border-gray-300 rounded-sm"
         >
           Login
-        </button>
+        </button> : <></>}
       </div> : <div>Header when user is logged In</div>}
     </header>
   );
