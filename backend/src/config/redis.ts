cf8121eedca8fa35e64052c3f337fe1d90c4c8e3
MahnoorSaleem <mhnrsaleem579@gmail.com>
2025-07-17 13:15:21 +0400

    added event listeners for redis

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -6,7 +6,21 @@ const redis = new Redis({
   host: config.REDIS_HOST,
   port: config.REDIS_PORT,
   password: config.REDIS_PASSWORD,
+  keepAlive: 1000,
 //   tls: {rejectUnauthorized: false,}, // Needed for Redis Cloud (SSL) - worked after commenting this
+});
+
+redis.on('connect', () => {
+  console.log('✅ Redis connected');
+});
+
+redis.on('error', (err) => {
+  console.error('❌ Redis error:', err.message);
+  // Optionally: implement retry logic, alerts, etc.
+});
+
+redis.on('close', () => {
+  console.warn('⚠️ Redis connection closed');
 });
 
 export default redis;
